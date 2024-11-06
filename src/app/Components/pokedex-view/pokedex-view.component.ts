34608f3b1ecd1b9b98a62fd117aa1f43cf44aea3
tabatanovillo239 <tabatanovillo239@gmail.com>
2024-11-06 01:59:45 +0000

    first commit

--- src/app/Components/pokedex-view/pokedex-view.component.ts
+++ src/app/Components/pokedex-view/pokedex-view.component.ts
@@ -1,15 +1,18 @@
+// src/app/Components/pokedex-view/pokedex-view.component.ts
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { PokedexComponent } from '../pokedex/pokedex.component';
 import { GraphComponent } from '../graph/graph.component';
 import { FormsModule } from '@angular/forms';
 import { PokemonModel } from '../../Models/Pokemon';
-
+import { forkJoin, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pokedex-view',
   standalone: true,
-  imports: [CommonModule, PokedexComponent, GraphComponent, FormsModule],
+  imports: [CommonModule, PokedexComponent, GraphComponent, FormsModule, HttpClientModule],
   templateUrl: './pokedex-view.component.html',
   styleUrls: ['./pokedex-view.component.css']
 })
@@ -21,24 +24,62 @@ export class PokedexViewComponent implements OnInit {
   searchTerm: string = '';
   searchResults: PokemonModel[] = [];
 
+  private baseUrl = 'https://pokeapi.co/api/v2/pokemon';
+  private spriteUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon';
+
+  constructor(private http: HttpClient) { }
+
   ngOnInit() {
     this.loadPokemonData();
   }
 
   loadPokemonData(): void {
     this.isLoading = true;
-    setTimeout(() => {
-      this.currentPokemon = new PokemonModel(
-        this.currentPokemonId,
-        this.getPokemonName(this.currentPokemonId),
-        this.getPokemonImageUrl(),
-        Math.floor(Math.random() * 100) + 50, // HP
-        Math.floor(Math.random() * 80) + 40,  // ATK
-        Math.floor(Math.random() * 70) + 30,  // DEF
-        this.getPokemonType(this.currentPokemonId)
-      );
-      this.isLoading = false;
-    }, 1000);
+    this.getPokemon(this.currentPokemonId).subscribe({
+      next: (pokemon) => {
+        this.currentPokemon = pokemon;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error loading Pokemon:', error);
+        this.isLoading = false;
+      }
+    });
+  }
+
+  getPokemon(id: number): Observable<PokemonModel> {
+    return this.http.get<any>(`${this.baseUrl}/${id}`).pipe(
+      map(response => {
+        return new PokemonModel(
+          response.id,
+          this.capitalizeFirstLetter(response.name),
+          `${this.spriteUrl}/${id}.png`,
+          response.stats.find((s: any) => s.stat.name === 'hp').base_stat,
+          response.stats.find((s: any) => s.stat.name === 'attack').base_stat,
+          response.stats.find((s: any) => s.stat.name === 'defense').base_stat,
+          response.types[0].type.name.toUpperCase()
+        );
+      })
+    );
+  }
+
+  private capitalizeFirstLetter(string: string): string {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  onSearch() {
+    if (this.searchTerm.length > 0) {
+      const searches = Array.from({ length: 151 }, (_, i) => i + 1)
+        .map(id => this.getPokemon(id));
+
+      forkJoin(searches).subscribe(pokemons => {
+        this.searchResults = pokemons
+          .filter(pokemon => pokemon.getNombre().toLowerCase().includes(this.searchTerm.toLowerCase()))
+          .slice(0, 5);
+      });
+    } else {
+      this.searchResults = [];
+    }
   }
 
   prevPokemon() {
@@ -55,6 +96,13 @@ export class PokedexViewComponent implements OnInit {
     }
   }
 
+  selectPokemon(pokemon: PokemonModel) {
+    this.currentPokemon = pokemon;
+    this.currentPokemonId = pokemon.getId();
+    this.searchTerm = '';
+    this.searchResults = [];
+  }
+
   isPrevDisabled(): boolean {
     return this.currentPokemonId <= 1;
   }
@@ -63,41 +111,7 @@ export class PokedexViewComponent implements OnInit {
     return this.currentPokemonId >= this.maxPokemonId;
   }
 
-  onSearch() {
-    if (this.searchTerm.length > 0) {
-      this.searchResults = Array.from({ length: 5 }, (_, i) =>
-        new PokemonModel(
-          i + 1,
-          `Pokemon ${i + 1}`,
-          `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png`,
-          Math.floor(Math.random() * 100) + 50,
-          Math.floor(Math.random() * 80) + 40,
-          Math.floor(Math.random() * 70) + 30,
-          this.getPokemonType(i + 1)
-        )
-      ).filter(pokemon => pokemon.getNombre().toLowerCase().includes(this.searchTerm.toLowerCase()));
-    } else {
-      this.searchResults = [];
-    }
-  }
-
-  selectPokemon(pokemon: PokemonModel) {
-    this.currentPokemon = pokemon;
-    this.currentPokemonId = pokemon.getId();
-    this.searchTerm = '';
-    this.searchResults = [];
-  }
-
   getPokemonImageUrl(): string {
-    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.currentPokemonId}.png`;
-  }
-
-  getPokemonName(id: number): string {
-    return `Pokemon ${id}`;
-  }
-
-  getPokemonType(id: number): string {
-    const types = ['Normal', 'Fire', 'Water', 'Electric', 'Grass', 'Ice', 'Fighting', 'Poison', 'Ground'];
-    return types[Math.floor(Math.random() * types.length)];
+    return `${this.spriteUrl}/${this.currentPokemonId}.png`;
   }
 }
